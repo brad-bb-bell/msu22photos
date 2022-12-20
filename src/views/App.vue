43191c72600b2c78456113c1f67fc31559be3090
brad-bb-bell <brad.bell5@montana.edu>
2022-12-20 13:21:35 -0700

    refactors text as an array of objects

--- src/views/App.vue
+++ src/views/App.vue
@@ -4,20 +4,16 @@
     <p
       class="absolute inset-0 flex items-center justify-center text-center text-4xl font-serif font-bold text-white tracking-wider"
     >
-      Welcome to MSU
+      An Extraordinary Year
     </p>
     <p
-      class="absolute inset-0 pt-20 flex items-center justify-center text-center text-2xl font-serif italic text-white tracking-wider"
+      class="absolute inset-0 pt-20 flex items-center justify-center text-center text-xl font-serif italic text-white tracking-wider"
     >
-      2022 Images of the Year
+      Favorite images of Montana State University's photographers from 2022
     </p>
   </div>
 
-  <TextBase
-    headline="Academic Life"
-    text="Academic Life Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-    laborum."
-  />
+  <TextBase :headline="this.text[0].headline" :text="this.text[0].text" />
 
   <div>
     <Container>
@@ -39,18 +35,7 @@
       />
     </Container>
 
-    <TextBase
-      headline="Campus Life"
-      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    />
-
-    <!-- <div>
-      <div class="container w-4/5 p-4 ml-auto mr-auto">
-        <div class="grid gap-2">
-          <ImageSingle :image="images[6].full" />
-        </div>
-      </div>
-    </div> -->
+    <TextBase :headline="this.text[1].headline" :text="this.text[1].text" />
 
     <Teleport to="body">
       <TheLightbox :images="images" :index="index" v-if="visible" @close="hide()" />
@@ -70,13 +55,31 @@ export default {
     TheLightbox,
     ImageDouble,
     ImageBase,
-    Container,
     TextBase,
+    Container,
   },
   data() {
     return {
       visible: false,
       index: 0,
+      text: [
+        {
+          headline: "Academic Life",
+          text: "This fall, MSU welcomed its second-largest incoming class in history at 3,752 new students for a total enrollment of 16,688. As Montana's premier university, MSU's annual research expenditures topped $200 million for the first time in 2022, topping an all-time high for the fourth straight year.",
+        },
+        {
+          headline: "Campus Life",
+          text: "From the start-of-the-academic year traditions like the lighting of the “M” on Mount Baldy and the energetic Catapalooza on MSU's Centennial Mall to end-of-semester commencement celebrations in the spring and fall, students took advantage of all that MSU and the greater Bozeman area had to offer in 2022.",
+        },
+        {
+          headline: "Scenic",
+          text: "Situated in the Gallatin Valley between the dramatic backdrops of the Bridger and Gallatin mountain ranges, the MSU campus is stunning to behold in every season. As they have for nearly 130 years, students and faculty in 2022 were able to enjoy the mountain town's crisp spring days, rich fall colors and deep winter snows, all while pursuing educational and recreational adventures. Opportunities to enjoy the landscape include everything from taking time to recharge and enjoy the views on or near campus to venturing out to ski, fish, hunt and hike near Bozeman.",
+        },
+        {
+          headline: "Athletics",
+          text: "2022 will go down in history as an unforgettable year for Bobcat Athletics. In FCS National Championship action, the Bobcats swept all titles at the men's and women's basketball Big Sky Tournament and the men's tennis championship. The men's cross-country team qualified to compete at NCAA Nationals in November, the same month that brought us a Cat-Griz week with a conclusive win in the Brawl of the Wild by the Bobcat football team. Bobcat fans turned out enthusiastically for the fun-filled ESPN College GameDay, which made its first-ever stop in Montana at this House of Blue and Gold. After an undefeated performance against the Big Sky Conference teams during the regular season, the Bobcats are this year's football conference co-champions and the team advanced to the FCS semifinals. No wonder we called it the Year of the Bobcat!",
+        },
+      ],
       images: [
         {
           full: "academic-life/academic-one.jpeg",
